cd281b59795bd36318c6a0f0fda0dbd0768d68c4
Iliyass Zamouri <iliyass.zamouri@gmail.com>
2022-03-12 18:59:09 +0100

    Info - Update -> UserController - Comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,116 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function info()
+    {
+        // getting authenticated user
+        $user = Auth::user();
+
+        // checking for auth status
+        if($user == null){
+
+            // constructing a response for error
+            $response = [
+                'status' => 'unauthenticated',
+                'msg' => 'Authentication is required to access this resource'
+            ];
+            return response($response, 403);
+
+        }
+
+        // constructing a response for success
+        $response = [
+            'status' => 'success',
+            'data' => $user
+        ];
+
+        // returing user info
+        return response($response, 200);
+
+    }
+
+    public function update(Request $request)
+    {
+        // getting the authenticated user
+        $user = Auth::user();
+
+        // checking if the response will change the email
+        if($request->email != null)
+        {
+            // validating the email
+            $request->validate(['email' => 'string']);
+
+            // checking if another user with this email already exists
+            // getting users where email match email passed in the request
+            // and the id is diffrent from the authenticated user
+            $usedEmail = User::where('email' == $request->email)->where('id', '<>' , $user->id)->get();
+
+            // checking the list if there is a record
+            if($usedEmail->count() != 0){
+                // returing an error response
+                return response([
+                    'status' => 'error',
+                    'msg' => 'email already been used'
+                ], 403);
+            }
+            // else changing the email to the request email
+            $user->email = $request->email;
+        }
+
+        // checking if the response will change the the phone number
+        if($request->phone != null)
+        {
+            // validating the email
+            $request->validate(['phone' => 'string']);
+
+            // checking if another user with this phone number already exists
+            // getting users where the phone number match phone number passed in the request
+            // and the id is diffrent from the authenticated user
+            $usedEmail = User::where('phone' == $request->phone)->where('id', '<>' , $user->id)->get();
+
+            // checking the list if there is a record
+            if($usedEmail->count() != 0){
+                // returing an error response
+                return response([
+                    'status' => 'error',
+                    'msg' => 'phone number already been used'
+                ], 403);
+            }
+            // else changing the phone number to the request phone number
+            $user->phone = $request->phone;
+        }
+
+        // checking if the response will change the first name
+        if($request->first_name != null){
+            // validating the first name
+            $request->validate(['first_name' => 'string']);
+            // changing the last name of the user to the requested one
+            $user->first_name = $request->first_name;
+        }
+        // checking if the response will change the first name
+        if($request->last_name != null){
+            // validating the last name
+            $request->validate(['last_name' => 'string']);
+            // changing the last name of the user to the requested one
+            $user->last_name = $request->last_name;
+        }
+
+        // saving the user with the new info
+        $user->save();
+
+        // constructing a response
+        $response = [
+            'status' => 'success',
+            'data' => $user
+        ];
+        // returning the response
+        return response($response, 201);
+
+    }
 }
