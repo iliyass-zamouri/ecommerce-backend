d339844da27bc457fdc17f1e8918422a0a32a9b2
Iliyass Zamouri <iliyass.zamouri@gmail.com>
2022-03-12 18:56:03 +0100

    Add Photo - Products - AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\City;
+use App\Models\Category;
 use App\Models\Photo;
 use App\Models\Specification;
 use Illuminate\Http\Request;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
 class AdminController extends Controller
@@ -14,18 +17,17 @@ class AdminController extends Controller
 
     public function storeProduct(Request $request)
     {
-
+        // validating the request
         $request->validate([
-            'slug' => 'string',
             'label' => 'required|string',
             'description' => 'required|string',
             'gender' => 'required|string',
             'mark_id' => 'required',
-            'specification' => 'array',
-            'photos' => 'array',
+            'specifications' => 'array',
             'category_id' => 'string'
         ]);
 
+        // creating new product record in the db
         $product = Product::create([
             'slug' => $request->slug == null ? Str::slug($request->label) : $request->slug,
             'label' => $request->label,
@@ -38,18 +40,82 @@ class AdminController extends Controller
         // looping specifications insertion [size, price]
         foreach($request->specifications as $spec){
             Specification::create([
-                'size' => $spec->size,
+                'size' => $spec['size'],
                 'product_id' => $product->id,
-                'price' => $spec->price,
+                'price' => $spec['price'],
             ]);
         }
 
+        // constructing a response
         $response = [
             'status' => 'success',
             'data' => $product
         ];
 
+        // returning the response with status code of 201
         return response($response,201);
+
+    }
+
+    public function addPhotoToProduct(Request $request)
+    {
+
+        // validating the id and the image file types, dimensions & size
+        $request->validate([
+            'product_id' => 'required|integer',
+            'label' => 'required|string',
+//            'photo' => 'required|image|mimes:jpg,png,jpeg,webp|max:2048|dimensions:min_width=800,min_height=495,max_width=800,max_height=495',
+             'photo' => 'mimes:jpeg,jpg,png,webp|max:2048'
+        ]);
+
+        // Getting the city with requested id
+        $product = Product::find($request->product_id);
+
+        // Getting the file from the request into a File object
+        $file = $request->file('photo');
+
+        $name = Str::slug($request->label);
+        // Changing the file name to the generated id and adding the file extension
+        $filename = $name.'.'.File::extension($file->getClientOriginalName());
+
+        // Uploading the file to the public path under the photos folder
+        $file->move(public_path().'/photos/products', $filename);
+
+
+        // Uploading the photo
+        $photo = Photo::create([
+            'label' => $request->label,
+            'file_name' => public_path().'/photos/products/' . $filename,
+            'product_id' => $product->id,
+        ]);
+
+        // Returning the response and the photo data
+        return response(['status' => 'success', 'data' => $photo ], 201);
+
+    }
+
+    // add new category of products
+    public function addCategory(Request $request){
+
+        //validating the request params
+        $request->validate([
+            'label' => $request->label
+        ]);
+
+        // creating the category
+        $category = Category::create([
+            'slug' => $request->slug == null ? Str::slug($request->label) : $request->slug,
+            'label' => $request->label,
+        ]);
+
+        // constructing a response
+        $response = [
+            'status' => 'success',
+            'data' => $category
+        ];
+
+        // returning a response with status code of 201
+        return response($response, 201);
 
     }
 
