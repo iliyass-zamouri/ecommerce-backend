4f31800de9417c4a1476be0257e2fab7e68f5756
Iliyass Zamouri <iliyass.zamouri@gmail.com>
2022-03-12 18:57:59 +0100

    show product with slug hadling error + commeting

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -10,11 +10,15 @@ class PublicController extends Controller
 
     public function allProducts()
     {
+        // getting all the products with thier all the relations
         $products = Product::allInfo()->get();
+
+        // constructing a response
         $response = [
             'status' => 'success',
             'data' => $products
         ];
+        // returning the response
         return response(
             $response, 200
         );
@@ -22,21 +26,40 @@ class PublicController extends Controller
 
     public function showProduct($slug)
     {
+        // finding the prodct with the given slug
         $product = Product::where('slug' , $slug)->allInfo()->get()->first();
+
+        if($product == null) {
+            // constructing a response for the error
+            $response = [
+                'status' => 'error',
+                'msg' => 'product with given slug not found'
+            ];
+            return response($response, 400);
+        }
+
+        // constructing a response for success
         $response = [
             'status' => 'success',
             'data' => $product
             ];
+
+        // returning the response
         return response($response, 200);
+
     }
 
     public function allCategories()
     {
+        // getting all the categories
         $categories = Category::all();
+
+        // constructing a response
         $response = [
             'status' => 'success',
             'data' => $categories
         ];
+        // returning the response
         return response(
             $response, 200
         );
