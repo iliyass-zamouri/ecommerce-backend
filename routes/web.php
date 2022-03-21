56c27597cc90c2c57b511150f70b1672660970bd
Iliyass Zamouri <iliyass.zamouri@gmail.com>
2022-03-21 13:59:18 +0100

    Revert "Laravel Breeze setup"
    This reverts commit 71902b0c44fcb5b0ff8d49aa182723ce72e3715d.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('welcome');
 });
-
-require __DIR__.'/auth.php';
